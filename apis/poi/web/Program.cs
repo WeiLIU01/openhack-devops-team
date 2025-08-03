b48c749281756ab4d75cf8c460348d578ee20286
Wei Liu <liu1156641488@gmail.com>
2025-08-03 20:48:23 +1000

    Recreate web project with traditional class structure to avoid conflicts

---
+++ apis/poi/web/Program.cs
@@ -0,0 +1,12 @@
+namespace WebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        app.MapGet("/", () => "Hello World!");
+        app.Run();
+    }
+} 
